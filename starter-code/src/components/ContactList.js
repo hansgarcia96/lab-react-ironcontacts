1467f6a6b1c16466671143d5874253bef130e8c8
Hans Garcia <hansgarcia96@gmail.com>
2019-09-21 16:20:09 -0400

    done

--- starter-code/src/components/ContactList.js
+++ starter-code/src/components/ContactList.js
@@ -5,12 +5,13 @@ class ContactList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: contacts
+      firstFiveList: contacts.slice(0, 5),
+      list : contacts.slice(5)
     };
   }
 
   showContacts = () => {
-    return this.state.list.slice(0, 6).map((eachContact, index) => {
+    return this.state.firstFiveList.map((eachContact, index) => {
       return (
         <tr>
           <td>
@@ -22,8 +23,78 @@ class ContactList extends React.Component {
           </td>
           <td>{eachContact.name}</td>
           <td>{eachContact.popularity}</td>
+          <td>
+            <button
+              onClick={() => {
+                this.deleteContact(index);
+              }}
+            >
+              Delete
+            </button>
+          </td>
         </tr>
       );
+    });
+  };
+
+  deleteContact = index => {
+    let copiedList = [...this.state.firstFiveList];
+
+    copiedList.splice(index, 1);
+
+    this.setState({
+        firstFiveList: copiedList
+    });
+  };
+
+  addRandomContact = e => {
+    // e.preventDefault();
+
+    let allContactsList = [...this.state.list];
+    let firstFive = [...this.state.firstFiveList]
+
+    let randomIndex = Math.floor(Math.random() * allContactsList.length - 1);
+    
+
+    firstFive.push(allContactsList[randomIndex]);
+
+    this.setState({
+        firstFiveList: firstFive
+    });
+  };
+
+  sortAlphabetically = () => {
+    let copiedList = [...this.state.firstFiveList];
+
+    copiedList.sort(function(a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+
+    this.setState({
+        firstFiveList: copiedList
+    });
+  };
+
+  sortPopularity = () => {
+    let copiedList = [...this.state.firstFiveList];
+
+    copiedList.sort(function(a, b) {
+      if (a.popularity > b.popularity) {
+        return -1;
+      }
+      if (a.popularity < b.popularity) {
+        return 1;
+      }
+      return 0;
+    });
+    this.setState({
+        firstFiveList: copiedList
     });
   };
 
@@ -32,13 +103,40 @@ class ContactList extends React.Component {
       <div>
         <h2>Ironcontacts</h2>
 
+        <button
+          onClick={() => {
+            this.addRandomContact();
+          }}
+        >
+          Add Random Contact
+        </button>
+
+        <button
+          onClick={() => {
+            this.sortAlphabetically();
+          }}
+        >
+          Sort Alphabetically
+        </button>
+
+        <button
+          onClick={() => {
+            this.sortPopularity();
+          }}
+        >
+          Sort Popularity
+        </button>
+
         <table>
-          <tr>
-            <td>Picture</td>
-            <td>Name</td>
-            <td>Popularity</td>
-          </tr>
-          {this.showContacts()}
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>{this.showContacts()}</tbody>
         </table>
       </div>
     );
